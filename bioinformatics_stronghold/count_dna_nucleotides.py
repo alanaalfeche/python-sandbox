69e8411f6915a31f72f0fad47c59027febf182a7
alanaalfeche <alanaalfeche@gmail.com>
2019-12-26 13:46:10 -0600

    Improve Naming Convention

--- bioinformatics_stronghold/count_dna_nucleotides.py
+++ bioinformatics_stronghold/count_dna_nucleotides.py
@@ -1,9 +1,9 @@
 sample_seq = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
 seq_count = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
 
-for element in sample_seq:
-    if element in seq_count:
-        seq_count[element] += 1
+for base in sample_seq:
+    if base in seq_count:
+        seq_count[base] += 1
 
 for key in seq_count:
     print(seq_count[key])
--- bioinformatics_stronghold/reverse_complement.py
+++ bioinformatics_stronghold/reverse_complement.py
@@ -1,9 +1,9 @@
-dna = "AAAACCCGGT"
+sample_seq = "AAAACCCGGT"
 complement = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
 reverse_complement = ""
 
-for ntd in dna:
-    ntd_complement = complement[ntd]
-    reverse_complement = ntd_complement + reverse_complement
+for base in sample_seq:
+    base_complement = complement[base]
+    reverse_complement = base_complement + reverse_complement
 
 print(reverse_complement)
