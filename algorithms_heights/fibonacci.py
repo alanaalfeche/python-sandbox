7176d54c69a7b192bfd4dc9993b6dcbf56a5f623
alanaalfeche <alanaalfeche@gmail.com>
2020-01-11 18:48:02 -0500

    Improve doc for fibonacci

--- algorithms_heights/fibonacci.py
+++ algorithms_heights/fibonacci.py
@@ -6,11 +6,39 @@ Fibonacci was introduced to the Western Mathematics by Leonardo of Pisa.
 Though it was first described in Indian Mathematics to study the growth of rabbits population. 
 '''
 
-# Fibonacci using bottom-up approach (for-loop) 
+# Fibonacci using recursion
+# Time Complexity: O(2^n)
+# Space Complexity: maximum depth of the recursion tree ~ n = O(n)
+def fibonacci_recursion(num):
+    if num <= 1: 
+        return num
+        
+    return fibonacci_recursion(num-1) + fibonacci_recursion(num-2)
+
+print(fibonacci_recursion(6))
+
+# Fibonacci using recursion -- dynamic programming: memoization approach
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+cache = {}
+def fibonacci_recursion_with_memoization(num):
+    if num in cache:
+        return cache[num]
+
+    if num <= 1:
+        result = num
+    else: 
+        result = fibonacci_recursion_with_memoization(num - 2) + fibonacci_recursion_with_memoization(num - 1)
+
+    cache[num] = result
+    return result
+
+print(fibonacci_recursion_with_memoization(6))
+# Fibonacci using for-loop -- dynammic programming: bottom-up approach
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 def fibonacci_for_loop(num):
-    if num == 0 or num == 1:
+    if num <= 1:
         return num
 
     else:
@@ -21,32 +49,3 @@ def fibonacci_for_loop(num):
         return c
 
 print(fibonacci_for_loop(6))
-
-# Fibonacci using recursion
-# Time Complexity: O(2^n)
-# Space Complexity: maximum depth of the recursion tree ~ n = O(n)
-def fibonacci_recursion(num):
-    if num == 0 or num == 1: 
-        return num
-        
-    return fibonacci_recursion(num-1) + fibonacci_recursion(num-2)
-
-print(fibonacci_recursion(6))
-
-# Fibonacci using recursion with memoization
-# Time Complexity: O(n)
-# Space Complexity: O(n)
-cache = {}
-def fibonacci_recursion_with_memoization(num):
-    if num in cache:
-        return cache[num]
-
-    if num == 0 or num == 1:
-        result = num
-    else: 
-        result = fibonacci_recursion_with_memoization(num - 2) + fibonacci_recursion_with_memoization(num - 1)
-
-    cache[num] = result
-    return result
-
-print(fibonacci_recursion_with_memoization(6))
