9ca15fc2b2291b86b7469fe7541ee08f0034f871
alanaalfeche <alanaalfeche@gmail.com>
2020-01-11 18:43:22 -0500

    Add for-loop and memoization implementation of Fibonacci

--- algorithms_heights/fibonacci.py
+++ algorithms_heights/fibonacci.py
@@ -1,9 +1,52 @@
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else: 
-        return fibonacci(n-1) + fibonacci(n-2)
+'''
+Fiboannci numbers forms a fibonacci sequence such that each number F(n) is the sum of the two preceding numbers.
+e.g. F(n) = F(n-1) + F(n-2) where F(0) = 0 and F(1) = 1
 
-print(fibonacci(6))
+Fibonacci was introduced to the Western Mathematics by Leonardo of Pisa. 
+Though it was first described in Indian Mathematics to study the growth of rabbits population. 
+'''
+
+# Fibonacci using bottom-up approach (for-loop) 
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+def fibonacci_for_loop(num):
+    if num == 0 or num == 1:
+        return num
+
+    else:
+        a = b = 1
+        for _ in range(3, num + 1):
+            c = a + b
+            a, b = b, c
+        return c
+
+print(fibonacci_for_loop(6))
+
+# Fibonacci using recursion
+# Time Complexity: O(2^n)
+# Space Complexity: maximum depth of the recursion tree ~ n = O(n)
+def fibonacci_recursion(num):
+    if num == 0 or num == 1: 
+        return num
+        
+    return fibonacci_recursion(num-1) + fibonacci_recursion(num-2)
+
+print(fibonacci_recursion(6))
+
+# Fibonacci using recursion with memoization
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+cache = {}
+def fibonacci_recursion_with_memoization(num):
+    if num in cache:
+        return cache[num]
+
+    if num == 0 or num == 1:
+        result = num
+    else: 
+        result = fibonacci_recursion_with_memoization(num - 2) + fibonacci_recursion_with_memoization(num - 1)
+
+    cache[num] = result
+    return result
+
+print(fibonacci_recursion_with_memoization(6))
