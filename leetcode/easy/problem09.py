e3bf3041ace120221b9b2339dc40dba744f32daf
alanaalfeche <alanaalfeche@gmail.com>
2020-07-09 18:52:04 -0500

    Improve solution to problem09

--- leetcode/easy/problem09.py
+++ leetcode/easy/problem09.py
@@ -16,3 +16,25 @@ def crooked_isPalindrome(num: int) -> bool:
     return False if num < 0 else num == int(str(num)[::-1])
 
 print(crooked_isPalindrome(313))
+
+def isPalindrome(num: int) -> bool:
+    """
+    Runtime: 56 ms, faster than 84.96% of Python3 online submissions for Palindrome Number.
+    Memory Usage: 13.7 MB, less than 85.66% of Python3 online submissions for Palindrome Number.
+    """
+    orig = num
+    if num < 0 or (num % 10 == 0 and num != 0):
+        """
+        Edge Case #1: number is negative e.g. -12 -> 21- --> Not palindrome
+        Edge Case #2: multiple of 0 e.g. 30 -> 03 --> Not palindrome
+        """
+        return False
+    
+    rev = 0
+    while num > 0:
+        rev = (10*rev) + num % 10
+        num //= 10
+
+    return rev == orig
+
+print(isPalindrome(313))
