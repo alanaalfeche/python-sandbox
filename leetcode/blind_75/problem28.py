11b34b0c09048f4240d984ffad1a0c2db76857e0
alanaalfeche <alanaalfeche@gmail.com>
2020-08-11 21:53:44 -0500

    Problem 28 Complete

---
+++ leetcode/blind_75/problem28.py
@@ -0,0 +1,26 @@
+'''
+Problem 121. Best Time to Buy and Sell Stock
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+'''
+
+def max_profit(prices):
+    # Solution authored by NDW: https://github.com/nolanwrightdev/blind-75-python/blob/master/problems/problem30.py
+    max_price = max_profit = float('-inf')
+    for price in prices[::-1]:
+        if price > max_price:
+            max_price = price
+        else:
+            max_profit = max(max_profit, max_price - price)
+    return max(max_profit, 0)
+
+prices = [7,1,5,3,6,4]
+expected = 5
+actual = max_profit(prices)
+print(actual)
+print(expected == actual) 
