cd3ef810db16c92a5a8c73ed65ec37c054bdbe6e
alanaalfeche <alanaalfeche@gmail.com>
2020-08-12 20:32:04 -0500

    Problem 102 Complete

---
+++ leetcode/medium/problem102.py
@@ -0,0 +1,38 @@
+"""
+Problem 102. Binary Tree Level Order Traversal
+https://leetcode.com/problems/implement-trie-prefix-tree/
+
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+"""
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def level_order(self, root: TreeNode) -> List[List[int]]:
+        # Runtime: 36 ms
+        # Memory Usage: 14.2 MB
+
+        if not root:
+            return []
+        
+        queue = [root]
+        result = []
+        
+        while queue:
+            parent = []
+            children = []
+            for node in queue:
+                parent.append(node.val)
+                if node.left:
+                    children.append(node.left)
+                if node.right:
+                    children.append(node.right)
+            result.append(parent)
+            queue = children
+        
+        return result
