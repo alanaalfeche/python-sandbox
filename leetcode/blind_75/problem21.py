9ff235f7e5eb40e24789c85c2463639bbc4e245b
alanaalfeche <alanaalfeche@gmail.com>
2020-08-13 23:13:28 -0500

    Problem 41 Complete

---
+++ leetcode/blind_75/problem21.py
@@ -0,0 +1,33 @@
+'''
+76. Minimum Window Substring
+https://leetcode.com/problems/minimum-window-substring/
+
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+'''
+
+def min_windows(s, t):
+    size = len(t)
+    t_dict = {c:1 for c in list(t)} # can also use collections.Counter(t)
+
+    result = s
+    x = 0
+    m = 2
+    for i, c in enumerate(s):
+        if c in t_dict.keys():
+            t_dict[c] += 1
+        if sum(t_dict.values()) == m * size:
+            temp = s[x:i+1]
+            print(temp)
+            if len(result) > len(temp):
+                result = temp
+            x = i
+            m += 1
+        
+    return result
+
+
+s = "ADOBECODEBANC"
+t = "ABC"
+expected = "BANC"
+actual = min_windows(s, t)
+print(actual == expected)
---
+++ leetcode/blind_75/problem41.py
@@ -0,0 +1,35 @@
+'''
+206. Reverse Linked List
+https://leetcode.com/problems/reverse-linked-list/
+
+Reverse a singly linked list.
+'''
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = None
+
+def reverse(head):
+    prev = None
+    
+    while head:
+        node = head
+        head = head.next
+        node.next = prev
+        prev = node
+
+    return prev
+
+
+last = None
+for i in range(5, 0, -1):
+    node = ListNode(i)
+    node.next = last
+    last = node
+
+answer = reverse(last)
+for _ in range(5):
+    print(answer.val)
+    answer = answer.next
