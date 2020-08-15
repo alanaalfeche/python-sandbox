9190c09a4b49647d3574e152525032c90ed9c3a0
alanaalfeche <alanaalfeche@gmail.com>
2020-08-15 14:19:19 -0500

    Problem 25 Complete

---
+++ leetcode/blind_75/problem25.py
@@ -0,0 +1,22 @@
+'''
+Problem 100. Same Tree
+https://leetcode.com/problems/same-tree/
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+'''
+
+
+def is_same_tree(p, q):     
+    # both None  
+    if not p and not q:
+        return True
+    # one is None
+    if not p or not q:
+        return False
+    # if root values are different
+    if p.val != q.val:
+        return False
+    return self.is_same_tree(p.right, q.right) and self.is_same_tree(p.left, q.left)
+    
