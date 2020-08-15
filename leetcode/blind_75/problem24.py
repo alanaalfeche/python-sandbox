db0e0d46d21f5b3e009ef7e16c7feba7ef1131d2
alanaalfeche <alanaalfeche@gmail.com>
2020-08-15 16:29:06 -0500

    Problem 24 Improvements

--- leetcode/blind_75/problem24.py
+++ leetcode/blind_75/problem24.py
@@ -10,12 +10,23 @@ The left subtree of a node contains only nodes with keys less than the node's ke
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
 '''
+
+
 def is_valid_bst(root):
-    def helper(node, lower = float('-inf'), upper = float('inf')):
-        if not node:
-            return True
-        val = node.val
-        if val <= lower or val >= upper:
+    
+    def traverse_inorder(root, inorder):
+        if root is None:
+            return
+
+        traverse_inorder(root.left, inorder)
+        inorder.append(root.val)
+        traverse_inorder(root.right, inorder)
+
+    inorder = []
+    traverse_inorder(root, inorder)
+
+    for i in range(1, len(inorder)):
+        if inorder[i-1] >= inorder[i]:
             return False
-        return helper(node.right, val, upper) and helper(node.left, lower, val)
-    return helper(root)
+
+    return True
