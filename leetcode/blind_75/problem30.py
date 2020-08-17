518d8c2412b5c9b9560a2a37c28c472665a03976
alanaalfeche <alanaalfeche@gmail.com>
2020-08-16 20:59:43 -0500

    Problem 30 Improvements

--- leetcode/blind_75/problem30.py
+++ leetcode/blind_75/problem30.py
@@ -7,8 +7,19 @@ Given a string, determine if it is a palindrome, considering only alphanumeric c
 
 
 def is_palindrome(s) -> bool:
-    text = ''.join([i.lower() for i in s if (i.isalpha() or i.isnumeric())])
-    return text == text[::-1]
+    # Authored By NDW: https://github.com/nolanwrightdev/blind-75-python/blob/master/problems/problem32.py
+    left, right = 0, len(s) - 1
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+        elif not s[right].isalnum():
+            right -= 1
+        elif s[left].lower() != s[right].lower():
+            return False
+        else:
+            left += 1
+            right -= 1
+    return True
 
 
 s = "A man, a plan, a canal: Panama"
