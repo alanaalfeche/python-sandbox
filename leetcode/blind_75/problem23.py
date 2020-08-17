9ee2b2e409311ffacbc5145f89f0f1d11cf79a5e
alanaalfeche <alanaalfeche@gmail.com>
2020-08-16 21:48:24 -0500

    Problem 23 Complete

---
+++ leetcode/blind_75/problem23.py
@@ -0,0 +1,30 @@
+'''
+Problem 91. Decode Ways
+https://leetcode.com/problems/decode-ways/
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+'''
+
+
+def num_decodings(s):
+    if not s:
+        return 0
+
+    cache = [0 for x in range(len(s) + 1)]
+
+    cache[0] = 1
+    cache[1] = 0 if s[0] == '0' else 1
+
+    for i in range(2, len(s) + 1):
+        if 0 < int(s[i-1:i]) <= 9:
+            cache[i] += cache[i-1]
+        if 10 <= int(s[i-2:i]) <= 26:   
+            cache[i] += cache[i-2]
+    
+    return cache[len(s)]
+
+
+s = '226'
+expected = 3
+actual = num_decodings(s)
+print(expected == actual)
