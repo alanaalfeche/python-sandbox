9dc4a5cb26b2f51299583aa88e29213e67ad617f
alanaalfeche <alanaalfeche@gmail.com>
2020-08-19 21:43:22 -0500

    Valid Parentheses Complete

--- leetcode/blind_75/problem07.py
+++ leetcode/blind_75/problem07.py
@@ -1,41 +1,31 @@
-'''
-Problem 7 - Valid Parentheses
+'''Problem 7: Valid Parentheses
+
 https://leetcode.com/problems/valid-parentheses/
 
 Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 '''
 
-def isValid(text: str) -> bool:
-    '''
-    Stack follows a LIFO - last in and first out pattern. 
-    We first traverse through the set of characters in a text. 
-    When an opening bracket is found, it is added to the stack.
-    When a closing bracket is found, the mapping is used to see if the last added character matches the accompanying pair of the closing bracket. If not, it returns False.
 
-    Time Complexity: O(n)
-    Space Complexity: O(n) for stack
+def is_valid(text):
+    '''We first traverse through the set of characters in a text. When an opening bracket is found, it is added to the stack which has a last in and first out pattern. 
+
+    When a closing bracket is found, the mapping is used to see if the last added character matches the accompanying pair of the closing bracket. If not, it returns False.
     '''
     mapping = {
         ')' : '(',
         '}' : '{',
         ']' : '['
     }
-    
     stack = []
-    
     for char in text:
-        if char in mapping: # add closing BRACKETS
+        if char in mapping:
             top_element = stack.pop() if stack else '#'
             if mapping[char] != top_element:
                 return False
-        
-        else: # add opening brackets
+        else:
             stack.append(char)
-            
-    # an empty stack returns False
-    # so we return the opposite of bool
     return not bool(stack)
 
-print(isValid('([{}])'))
-print(isValid('()[]{}'))
 
+print(is_valid('([{}])'))
+print(is_valid('()[]{}'))
