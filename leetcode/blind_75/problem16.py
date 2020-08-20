7fde88f2821649d7738ebbfdcb4df71b843b949f
alanaalfeche <alanaalfeche@gmail.com>
2020-08-20 01:12:26 -0500

    Merge Intervals Complete

--- leetcode/blind_75/problem16.py
+++ leetcode/blind_75/problem16.py
@@ -1,5 +1,5 @@
-'''
-Problem 16. Merge Intervals
+'''Problem 16. Merge Intervals
+
 https://leetcode.com/problems/merge-intervals/
 
 Given a collection of intervals, merge all overlapping intervals.
@@ -10,19 +10,25 @@ def merge(intervals):
     # solution authored by NDW: https://github.com/nolanwrightdev/blind-75-python/blob/master/problems/problem17.py
     if not intervals:
         return []
-    '''
-    Sort is in-place (i.e. the list itself is modified) and stable (i.e. the order of two equal elements is maintained).
+    '''Sort is in-place (i.e. the list itself is modified) and stable (i.e. the order of two equal elements is maintained).
+
     Passing lambda as key parameter creates an anonymous fn that is applied to each list item.
+
     The expression `lambda parameters: expression` yields a function object like a function object defined with:
+
     def <lambda>(parameters):
         return expression
 
-    https://docs.python.org/3/reference/expressions.html#lambda
+    This algorithm start by sorting the intervals on the [i][j] ith term. We then compare the last j value to the start value of an interval [start, end] from our traversal.
+
+    If the start value is less than j, then it can be merged. We then used max function to determine whether we use the j or the end value to fully merge an interval.
+
+    Otherwise, just append start,end to the result. 
     '''
     if not intervals:
         return []
 
-    intervals.sort(key=lambda x: x[0])
+    intervals.sort(key = lambda x: x[0])
     result = [intervals[0]]
     for interval in intervals[1:]:
         start, end = interval
