f266e36e01987ba0740d11de15e7928d0ec98b02
alanaalfeche <alanaalfeche@gmail.com>
2020-08-19 21:29:16 -0500

    Remove Nth Node from End of List Complete

--- leetcode/blind_75/problem06.py
+++ leetcode/blind_75/problem06.py
@@ -1,23 +1,25 @@
-"""Problem 6 - Remove Nth Node from End of List
+'''Problem 6: Remove Nth Node from End of List
+
 https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
-Runtime: 36 ms, faster than 58.41% of Python3 online submissions for Remove Nth Node From End of List.
-Memory Usage: 13.8 MB, less than 61.47% of Python3 online submissions for Remove Nth Node From End of List.
-
 Given a linked list, remove the n-th node from the end of list and return its head.
-"""
+'''
+
+
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
-def solution(head: ListNode, n: int) -> ListNode:
-    node = ListNode(0)
-    node.next = head
-    fast = slow = node
+def remove_nth_from_end(head, n):
+    fast = slow = head
 
     for _ in range(n):
         fast = fast.next
+
+    # edge case: [1]
+    if not fast:
+        return head.next
 
     while fast.next:
         fast = fast.next
@@ -27,6 +29,7 @@ def solution(head: ListNode, n: int) -> ListNode:
 
     return node.next
 
+
 last = None
 for i in range(5, 0, -1):
     node = ListNode(i)
@@ -35,5 +38,4 @@ for i in range(5, 0, -1):
 
 # This will create the following input [1, [2, [3, [4, [5, None]]]]]
 # Authored by NDW: https://github.com/nolanwrightdev/blind-75-python/blob/master/tests/test_problem06.py
-
-print(solution(last, 2))
+remove_nth_from_end(last, 2)
