cc172b9e9e757c74cb7704332e258b762096ca43
alanaalfeche <alanaalfeche@gmail.com>
2020-08-20 08:06:44 -0500

    Climb Stairs Complete

--- leetcode/blind_75/problem19.py
+++ leetcode/blind_75/problem19.py
@@ -1,15 +1,19 @@
-'''
-Problem 70. Climbing Stairs
-https://leetcode.com/problems/group-anagrams/
+'''Problem 70: Climbing Stairs
+
+https://leetcode.com/problems/climbing-stairs/
 
 You are climbing a stair case. It takes n steps to reach to the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 '''
+
+
 cache = {}
-def climb_stairs_memoization(n):
-    # dynamic programming - recursion with memoization
+def climb_stairs(n):
+    '''This is similar to fibonacci problem where each unique ways to climb a step n is dependent on the unique ways to climb (n-2) + (n-1).
     
+    This uses recursion with memoization to remove redundancy that we see often with regular fibonacci-recursion problems.
+    '''
     if n in cache:
         return cache[n]
 
@@ -18,24 +22,13 @@ def climb_stairs_memoization(n):
     elif n == 2:
         result = 2
     else:
-        result = climb_stairs_memoization(n-2) + climb_stairs_memoization(n-1)
+        result = climb_stairs(n-2) + climb_stairs(n-1)
 
     cache[n] = result
     return result
 
-def climb_stairs_bottom_up(n):
-    # dynamic programming - bottom-up solution
-    if n <= 1:
-        return 1
-    else:
-        a = 1
-        b = 2
-        for _ in range(3, n + 1):
-            c = a + b 
-            a, b = b, c
-        return c
 
-sample = 3
-actual = climb_stairs_memoization(sample)
-actual2 = climb_stairs_bottom_up(sample)
-print(actual == actual2)
+n = 3
+expected = 3
+actual = climb_stairs(n)
+print(actual == expected)
