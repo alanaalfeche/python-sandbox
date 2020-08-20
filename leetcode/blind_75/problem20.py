0966c8115c3298b6ac2d873099f4b6dee369186f
alanaalfeche <alanaalfeche@gmail.com>
2020-08-20 08:15:13 -0500

    Set Zeroes Complete

--- leetcode/blind_75/problem20.py
+++ leetcode/blind_75/problem20.py
@@ -1,11 +1,19 @@
-'''
-73. Set Matrix Zeroes
+'''Problem 73: Set Matrix Zeroes
+
 https://leetcode.com/problems/set-matrix-zeroes/
 
 Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
 '''
 
 def set_zeroes(matrix):
+    '''This algorithm uses set() to track the row and column indices at which 0 is found. This is to ensure there is no redundancy when we set these indices to 0.
+
+    Once we have the list of row and column indices:
+    For rows, we iterate from 0 -> n (number of columns) and set M[r][n] = 0 --> horizontal zeroing
+    For columns, we iterate from 0 -> m (number of rows) and set M[m][c] = 0 --> vertical zeroing
+
+    This is an n^2 solution.
+    '''
     m = len(matrix)
     n = len(matrix[0])
     
@@ -40,5 +48,4 @@ expected = [
   [1,0,1]
 ]
 actual = set_zeroes(matrix)
-print(actual)
 print(actual == expected)
