b3d8cb46bfd92e0bec57ea4c4d2226061ad6ff84
alanaalfeche <alanaalfeche@gmail.com>
2020-08-20 00:48:20 -0500

    Maximum Subarray Complete

--- leetcode/blind_75/problem13.py
+++ leetcode/blind_75/problem13.py
@@ -1,15 +1,16 @@
-'''
-Problem 13. Maximum Subarray
+'''Problem 13: Maximum Subarray
+
 https://leetcode.com/problems/maximum-subarray/
 
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 '''
 
+
 def max_sub_array(nums):
-    best_sum = curr_sum = nums[0]
+    best_sum = local_sum = nums[0]
     for num in nums[1:]:
-        curr_sum = max(num, curr_sum + num)
-        best_sum = max(curr_sum, best_sum)
+        local_sum = max(num, local_sum + num)
+        best_sum = max(local_sum, best_sum)
     return best_sum
 
 
