9de9aaf4278a5480fa3b2e8f0ded3051d95a7be3
alanaalfeche <alanaalfeche@gmail.com>
2020-08-19 19:05:24 -0500

    Length of Longest Substring Complete

--- leetcode/blind_75/problem02.py
+++ leetcode/blind_75/problem02.py
@@ -1,49 +1,34 @@
-"""Blind Curated 75 - Problem 2: Longest Substring Without Repeating Characters
+'''Problem 2: Longest Substring Without Repeating Characters
+
 https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
 Given a string, find the length of the longest substring without repeating characters.
-"""
-def solution(text: str) -> int:
-    """Returns the maximum substring length without repeating character.
-    First, it iterates through the char of a string, adding the char - index pair to a hash map. 
-    If char is found, and the index of the current char is greater than or equal to the start index, then we update the start index to the current char index + 1. 
-    This is used to determine the start window where no repeating character has been found in a substring. 
-    Otherwise, return the max of tne current length of substring or the current index minus the start index + 1 because it is 0 based for loop.
+'''
 
-    Time Complexity: n
-    Space Complexity: min(m, n) where n is the size of string and m is the size of charset/alphabet 
-    Runtime: 92 ms, faster than 29.29% of Python3 online submissions for Longest Substring Without Repeating Characters.
-    Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Longest Substring Without Repeating Characters.
-    """
-    char_map = {}
-    len_pref = 0
-    start = 0
 
-    for i, char in enumerate(text):
-        if char in char_map and char_map[char] >= start:
-            start = char_map[char] + 1
-        else:
-            len_pref = max(len_pref, i - start + 1)
-        
-        char_map[char] = i
-        
-    return len_pref
-
-print(solution("tmmzuxt"))
-
-# Uses list instead of hashmap 
-def longest_substring(text):
-    alist = []
-    temp = 0
+def length_of_longest_substring(text):
+    '''This is a sliding window problem. We start with creating a hashmap which will store char - index as key - value pair, respecitly. 
     
-    for i, char in enumerate(text):
-        if char not in alist:
-            alist.append(char)
-            temp += 1
-        else:
-            temp = max(temp, len(alist))
-            alist = []
-            alist.append(char)
-    return temp - 1
+    The start variable determines the index at which no substring has repeating characters whereas the i variable will traverse through the list, expanding the window. 
+    
+    If a char at index i is found in the hash map then we update the start index to the current index i + 1 (0-based index). 
 
-print(longest_substring("tmmzuxt"))
+    Otherwise, we compare the current max length to current index - start index + 1 (0-based index).
+
+    The `and visited[char] >= start` case is added here because we do not want to go back to an earlier starting point if start index is less than current index. 
+    '''
+    visited = {}
+    max_length = start = 0
+    for i, char in enumerate(s):
+        if char in visited and visited[char] >= start:
+            start = visited[char] + 1
+        else:
+            max_length = max(max_length, i - start + 1)
+        visited[char] = i
+    return max_length
+
+
+text = 'tmmzuxt'
+expected = 5
+actual = solution(text)
+print(expected == actual)
