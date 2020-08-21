076221f81370b558ebd7e7fa57ed5a850e28b8ef
alanaalfeche <alanaalfeche@gmail.com>
2020-08-21 07:04:16 -0500

    Implement Trie -- Need Review

--- leetcode/blind_75/problem46.py
+++ leetcode/blind_75/problem46.py
@@ -1,25 +1,19 @@
-"""
-Problem 208 - Implement Trie (Prefix Tree)
+'''Problem 208: Implement Trie (Prefix Tree)
+
 https://leetcode.com/problems/implement-trie-prefix-tree/
 
 Implement a trie with insert, search, and startsWith methods.
-"""
+'''
 
 
 class Trie:
 
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.trie = {}
 
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
         t = self.trie
         for w in word:
             if w not in t:
@@ -27,10 +21,8 @@ class Trie:
             t = t[w]
         t['#'] = '#'
 
+
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
         t = self.trie
         for w in word:
             if w not in t:
@@ -42,9 +34,6 @@ class Trie:
         
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
         t = self.trie
         for w in prefix:
             if w not in t:
