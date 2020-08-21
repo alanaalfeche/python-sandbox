5b3030330e6bcb621689af593f18d04499ddd0d9
alanaalfeche <alanaalfeche@gmail.com>
2020-08-20 21:45:21 -0500

    Minimum Window Substring Complete

--- leetcode/blind_75/problem21.py
+++ leetcode/blind_75/problem21.py
@@ -1,29 +1,42 @@
-'''
-76. Minimum Window Substring
+'''Problem 76: Minimum Window Substring
+
 https://leetcode.com/problems/minimum-window-substring/
 
 Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
 '''
+from collections import Counter
+
 
 def min_windows(s, t):
-    size = len(t)
-    t_dict = {c:1 for c in list(t)}
+    '''This algorithm uses sliding window to find the minimum substring in which all characters in T is satisfied. 
 
-    result = s
-    x = 0
-    m = 2
-    for i, c in enumerate(s):
-        if c in t_dict.keys():
-            t_dict[c] += 1
-        if sum(t_dict.values()) == m * size:
-            temp = s[x:i+1]
-            print(temp)
-            if len(result) > len(temp):
-                result = temp
-            x = i
-            m += 1
-        
-    return result
+    It starts by creating a hashmap for string T and initializing a missing variable to store the number of missing characters. 
+
+    Starting at index 0, we let r traverse through the list, checking if current char is found in the hashmap and decrementing missing if it is found. 
+
+    When we find all the missing character, we go inside the nested while loop to move l as close to r without incrementing missing. 
+
+    We also set the best start and end indices inside this while loop which we ultimately return as the minimum substring.
+    '''
+    need = Counter(t)
+    missing = len(t)
+
+    l, r, i, j = 0, 0, 0, 0
+    while r < len(s):
+        if need[s[r]] > 0: # subtract if char in s satisfy a char in t
+            missing -= 1
+        need[s[r]] -= 1
+        r += 1
+
+        while missing == 0: # contract l until we find start missing a char in t
+            if j == 0 or r - l < j - i: 
+                i, j = l, r
+            need[s[l]] += 1
+            if need[s[l]] > 0: # if char in t is found then break out from the while loop
+                missing += 1
+            l += 1 # contract l until a missing char is found
+
+    return s[i:j]
 
 
 s = "ADOBECODEBANC"
