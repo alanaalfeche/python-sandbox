7bbe8fed0ee390b48632d488ea0cd328f932a258
alanaalfeche <alanaalfeche@gmail.com>
2020-08-20 19:05:27 -0500

    Construct Binary Tree from Preorder and Inorder Traversal Complete

--- leetcode/blind_75/problem27.py
+++ leetcode/blind_75/problem27.py
@@ -1,5 +1,5 @@
-'''
-105. Construct Binary Tree from Preorder and Inorder Traversal
+'''Probelm 105: Construct Binary Tree from Preorder and Inorder Traversal
+
 https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
 
 Given preorder and inorder traversal of a tree, construct the binary tree.
@@ -14,9 +14,14 @@ class TreeNode:
 
 
 def build_tree(preorder, inorder):
+    '''The root is found at the first index in preorder. 
+    The values to the left of the root in inorder is the left subtree.
+    The values to the right of the root in inorder is the right subtree.
+    '''
     if inorder:
         idx = inorder.index(preorder.pop(0))
         root = TreeNode(inorder[idx])
         root.left(self.build_tree(preorder, inorder[:idx]))
         root.right(self.build_tree(preorder, inorder[idx+1:]))
         return root
+        
