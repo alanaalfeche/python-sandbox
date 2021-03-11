c159dbd59c570ed83fc1c422bdb768be5ab1d9f5
alanaalfeche <alanaalfeche@gmail.com>
2021-03-11 07:28:57 -0600

    Add 176. Second Highest Salary

--- leetcode/facebook/sql/176.sql
+++ leetcode/facebook/sql/176.sql
@@ -17,8 +17,10 @@ For example, given the above Employee table, the query should return 200 as the 
 | 200                 |
 +---------------------+
 """
+-- By encapsulating the query with outer select, 
+-- the query returns NULL value if inner select returns 0 matching rows
 SELECT(
-    SELECT DISTINCT Salary
+    SELECT DISTINCT Salary -- We also added a distinct for cases where salary occurs more than once
     FROM Employee
     ORDER BY Salary DESC
     LIMIT 1 OFFSET 1
