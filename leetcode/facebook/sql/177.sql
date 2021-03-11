902451aff92b2e922340b0037923dc9732882589
alanaalfeche <alanaalfeche@gmail.com>
2021-03-11 07:32:08 -0600

    Add 175. Combine Two Tables

--- leetcode/facebook/sql/177.sql
+++ leetcode/facebook/sql/177.sql
@@ -25,6 +25,7 @@ AddressId is the primary key column for this table.
 Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
 FirstName, LastName, City, State
 """
-SELECT p.FirstName, p.LastName, a.City, a.State
-FROM Person p
-LEFT JOIN Address a on p.PersonId = a.PersonId
+-- We wanted a left join on this query because we want null for person that doesn't have address
+SELECT FirstName, LastName, City, State
+FROM Person 
+LEFT JOIN Address on Person.PersonId = Address.PersonId
