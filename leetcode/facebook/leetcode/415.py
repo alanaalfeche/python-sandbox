e1a13c924e65dc1e22a671244068365c9d0cfff7
alanaalfeche <alanaalfeche@gmail.com>
2021-03-23 20:00:05 -0500

    Add 415. Add String

--- leetcode/facebook/leetcode/415.py
+++ leetcode/facebook/leetcode/415.py
@@ -7,9 +7,28 @@ Note:
     3. Both num1 and num2 does not contain any leading zero.
     4. You must not use any built-in BigInteger library or convert the inputs to integer directly.
 """
-def addStrings(self, num1: str, num2: str) -> str:
-    pass
+def addStrings(num1: str, num2: str) -> str:
+    res = []
 
-expected = "3"
-actual = addStrings("1", "2")
+    carry = 0
+    p1 = len(num1) - 1
+    p2 = len(num2) - 1
+    while p1 >= 0 or p2 >= 0:
+        x1 = ord(num1[p1]) - ord('0') if p1 >= 0 else 0
+        x2 = ord(num2[p2]) - ord('0') if p2 >= 0 else 0
+
+        value = (x1 + x2 + carry) % 10
+        carry = (x1 + x2 + carry) // 10
+
+        res.append(value)
+        p1 -= 1
+        p2 -= 1
+
+    if carry:
+        res.append(carry)
+
+    return ''.join(str(x) for x in res[::-1])
+
+expected = "108"
+actual = addStrings("9", "99")
 print(actual == expected)
