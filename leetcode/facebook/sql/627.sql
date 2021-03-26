e4d00fe53352eb3ffcbf8ebf08978b95cca10095
alanaalfeche <alanaalfeche@gmail.com>
2021-03-25 20:39:15 -0500

    Add 627. Swap Salary

---
+++ leetcode/facebook/sql/627.sql
@@ -0,0 +1,29 @@
+""" 627. Swap Salary
+Write an SQL query to swap all 'f' and 'm' values with a single update statement and no intermediate temp table(s).
+
+Salary table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | m   | 2500   |
+| 2  | B    | f   | 1500   |
+| 3  | C    | m   | 5500   |
+| 4  | D    | f   | 500    |
++----+------+-----+--------+
+
+Result table:
++----+------+-----+--------+
+| id | name | sex | salary |
++----+------+-----+--------+
+| 1  | A    | f   | 2500   |
+| 2  | B    | m   | 1500   |
+| 3  | C    | f   | 5500   |
+| 4  | D    | m   | 500    |
++----+------+-----+--------+
+"""
+UPDATE Salary
+SET
+    sex = CASE sex
+        WHEN 'f' THEN 'm'
+        ELSE 'f'
+    END;
