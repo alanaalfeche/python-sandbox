6e9d398c29980af22b1ad3d7f057a246cd7692ce
alanaalfeche <alanaalfeche@gmail.com>
2021-03-27 10:07:38 -0500

    Add 1596. The Most Frequently Ordered Products for Each Customer

---
+++ leetcode/facebook/sql/1596.sql
@@ -0,0 +1,72 @@
+""" 1596. The Most Frequently Ordered Products for Each Customer
+ 
+Write an SQL query to find the most frequently ordered product(s) for each customer.
+
+The result table should have the product_id and product_name for each customer_id who ordered at least one order. 
+
+Return the result table in any order.
+
+Customers
++-------------+-------+
+| customer_id | name  |
++-------------+-------+
+| 1           | Alice |
+| 2           | Bob   |
+| 3           | Tom   |
+| 4           | Jerry |
+| 5           | John  |
++-------------+-------+
+
+Orders
++----------+------------+-------------+------------+
+| order_id | order_date | customer_id | product_id |
++----------+------------+-------------+------------+
+| 1        | 2020-07-31 | 1           | 1          |
+| 2        | 2020-07-30 | 2           | 2          |
+| 3        | 2020-08-29 | 3           | 3          |
+| 4        | 2020-07-29 | 4           | 1          |
+| 5        | 2020-06-10 | 1           | 2          |
+| 6        | 2020-08-01 | 2           | 1          |
+| 7        | 2020-08-01 | 3           | 3          |
+| 8        | 2020-08-03 | 1           | 2          |
+| 9        | 2020-08-07 | 2           | 3          |
+| 10       | 2020-07-15 | 1           | 2          |
++----------+------------+-------------+------------+
+
+Products
++------------+--------------+-------+
+| product_id | product_name | price |
++------------+--------------+-------+
+| 1          | keyboard     | 120   |
+| 2          | mouse        | 80    |
+| 3          | screen       | 600   |
+| 4          | hard disk    | 450   |
++------------+--------------+-------+
+
+Result table:
++-------------+------------+--------------+
+| customer_id | product_id | product_name |
++-------------+------------+--------------+
+| 1           | 2          | mouse        |
+| 2           | 1          | keyboard     |
+| 2           | 2          | mouse        |
+| 2           | 3          | screen       |
+| 3           | 3          | screen       |
+| 4           | 1          | keyboard     |
++-------------+------------+--------------+
+"""
+WITH cte AS (
+    SELECT customer_id, product_id, COUNT(product_id) as purchase_count
+    FROM orders 
+    GROUP BY customer_id, product_id
+)
+
+SELECT a.customer_id, a.product_id, p.product_name
+FROM cte a
+JOIN products p USING (product_id) -- need to be in ()
+WHERE purchase_count IN (
+    SELECT 
+        MAX(purchase_count)
+    FROM cte b
+    WHERE a.customer_id=b.customer_id
+)
