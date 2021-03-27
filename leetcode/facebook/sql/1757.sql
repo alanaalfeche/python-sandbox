d6f28a971735655c3a411953afd95e20b9842596
alanaalfeche <alanaalfeche@gmail.com>
2021-03-27 10:37:41 -0500

    Add 1757. Recyclable and Low Fat Products

---
+++ leetcode/facebook/sql/1757.sql
@@ -0,0 +1,36 @@
+""" 1757. Recyclable and Low Fat Products
+
+Write an SQL query to find the ids of products that are both low fat and recyclable.
+
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+
+Result table:
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+"""
+SELECT product_id
+FROM products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+-- Follow Up: Write a query to get the percentage of certain products
+SELECT ROUND(
+        AVG(CASE 
+                WHEN low_fats = 'Y' OR recyclable = 'Y' 
+                THEN 1 
+                ELSE 0 
+            END)
+        ,2) AS PERCENTAGE
+FROM products
