70d78208efa8099c6bb6a925d3c0bc5ca00efce5
alanaalfeche <alanaalfeche@gmail.com>
2021-03-27 22:40:17 -0500

    Add 371. Sum of Two Integers

--- leetcode/facebook/leetcode/371.py
+++ leetcode/facebook/leetcode/371.py
@@ -4,9 +4,17 @@ Given two integers a and b, return the sum of the two integers without using the
 """
 
 def getSum(a: int, b: int) -> int:
-    mask = 0xffffffff
+    """ Why mask?
+    Need mask because Python no longer use 8-bit numbers, but INFINITE number of bits
+    Useful for non-negative number, but a negative number will have INFINITE number of bits
+    Line 12 creates a 32 bit mask of 1's: x & 1 = x
+    """
+    mask = 0xffffffff 
     while(b & mask > 0):
         a, b = a ^ b, (a & b) << 1
+
+    # if b > 0, the carry bit is "finished". 
+    # if not, it will continue until it exceeds the 32 bit mask to end while loop
     return (a & mask) if b > 0 else a
 
 
