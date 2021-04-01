ab1efcbdbe3f868a1b76dd75aa0855665dadc21d
alanaalfeche <alanaalfeche@gmail.com>
2021-03-31 20:30:09 -0500

    Add 938. Range Sum of BST

---
+++ facebook/leetcode/algorithms/938.py
@@ -0,0 +1,21 @@
+"""938. Range Sum of BST
+Given the root node of a binary search tree,
+return the sum of values of all nodes with a value in the range [low, high].
+
+"""
+def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+    self.range_sum = 0
+
+    def dfs(node):
+        if node:
+            if low <= node.val <= high:
+                self.range_sum += node.val
+                
+            if node.val > low:
+                dfs(node.left)
+
+            if node.val < high:
+                dfs(node.right)
+
+    dfs(root)
+    return self.range_sum
