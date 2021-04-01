748f8d11b486b3b96133d50119c48cdeb279f929
alanaalfeche <alanaalfeche@gmail.com>
2021-04-01 13:24:09 -0500

    Update Problem 102. Binary Tree Level Order Traversal

--- leetcode/medium/problem102.py
+++ leetcode/medium/problem102.py
@@ -1,19 +1,14 @@
-'''Problem 102. Binary Tree Level Order Traversal
-
+"""Problem 102. Binary Tree Level Order Traversal
 https://leetcode.com/problems/binary-tree-level-order-traversal/
 
 Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
-'''
-
-
+"""
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
     def level_order(self, root: TreeNode) -> List[List[int]]:
         if not root:
