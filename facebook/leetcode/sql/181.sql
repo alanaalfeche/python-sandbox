a82a2bd74160ad51cf9b472efca82a873214d480
alanaalfeche <alanaalfeche@gmail.com>
2021-04-01 07:56:50 -0500

    Update 181. Employees Earning More Than Their Managers

--- facebook/leetcode/sql/181.sql
+++ facebook/leetcode/sql/181.sql
@@ -1,6 +1,8 @@
 """181. Employees Earning More Than Their Managers
-The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+https://leetcode.com/problems/employees-earning-more-than-their-managers/
 
+The Employee table holds all employees including their managers.
+Every employee has an Id, and there is also a column for the manager Id.
 +----+-------+--------+-----------+
 | Id | Name  | Salary | ManagerId |
 +----+-------+--------+-----------+
@@ -9,8 +11,9 @@ The Employee table holds all employees including their managers. Every employee 
 | 3  | Sam   | 60000  | NULL      |
 | 4  | Max   | 90000  | NULL      |
 +----+-------+--------+-----------+
-Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
 
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers.
+For the above table, Joe is the only employee who earns more than his manager.
 +----------+
 | Employee |
 +----------+
