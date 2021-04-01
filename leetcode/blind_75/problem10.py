38b274d92ab489e744a59238f2df0369beccb7c0
alanaalfeche <alanaalfeche@gmail.com>
2021-04-01 11:09:17 -0500

    Update Problem 39: Combination Sum

--- leetcode/blind_75/problem10.py
+++ leetcode/blind_75/problem10.py
@@ -1,21 +1,21 @@
-'''Problem 39: Combination Sum
-
+"""Problem 39: Combination Sum
 https://leetcode.com/problems/combination-sum/
 
-Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target),
+find all unique combinations in candidates where the candidate numbers sums to target.
 
 The same repeated number may be chosen from candidates unlimited number of times.
-'''
+"""
 
 
 def combination_sum(candidates, target):
-    ''' 0   1    2       3          4                  5
+    """ 0   1    2       3          4                  5
     1  [ ] [1] [1,1] [1, 1, 1] [1, 1, 1, 1]     [1, 1, 1, 1, 1]
     2  [ ] [ ] [2]   [2, 1]    [2, 2] [2, 1, 1] [2, 1, 1, 1] [2, 2, 1]
     3  [ ] [ ] [ ]   [3]       [3, 1]           [3, 1, 1] [3, 2] 
 
     All unique combinations are found in the last columns. 
-    '''
+    """
     # like line 12, we need to create a dp with targets + 1 to account for 0
     dp = [[] for _ in range(target+1)]
 
@@ -32,16 +32,15 @@ def combination_sum(candidates, target):
 
 
 candidates = [2,3,6,7]
-'''0    1   2   3    4     5      6    |    7
+"""0    1   2   3    4     5      6    |    7
 2 [ ]  [ ] [2]  x  [2,2]   x   [2,2,2] |    x
 3 [ ]  [ ] [ ] [3]   x   [3,2] [3,3]   | [3,2,2]
 6 [ ]  [ ] [ ] [ ]  [ ]   [ ]    [6]   |    x
 7 [ ]  [ ] [ ] [ ]  [ ]   [ ]    [ ]   | [7]
-'''
+"""
 target = 7
 expected = [
   [2,2,3],
   [7]
 ]       
-actual = combination_sum(candidates, target)
-print(expected == actual)
+assert combination_sum(candidates, target) == expected
