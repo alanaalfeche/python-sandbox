987be6695ac3198387c3fc34079155268bbc67c4
alanaalfeche <alanaalfeche@gmail.com>
2021-04-01 12:41:32 -0500

    Update 560. Subarray Sum Equals K

--- facebook/leetcode/algorithms/560.py
+++ facebook/leetcode/algorithms/560.py
@@ -10,13 +10,13 @@ def subarraySum(nums: List[int], k: int) -> int:
     sum_freq = {}
     sum_freq[0] = 1 # For instance where num in nums = k
 
-    prefix_sums = 0
+    cum_sum = 0
     k_count = 0
 
     for num in nums:
-        prefix_sums += num
-        k_count += sum_freq.get(prefix_sums-k, 0)
-        sum_freq[prefix_sums] = sum_freq.get(prefix_sums, 0) + 1     
+        cum_sum += num
+        k_count += sum_freq.get(cum_sum-k, 0)
+        sum_freq[cum_sum] = sum_freq.get(cum_sum, 0) + 1     
 
     return k_count
 
