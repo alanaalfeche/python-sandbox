8c3dd23b6f90cf2aab00507b50361a7ec2b35c42
alanaalfeche <alanaalfeche@gmail.com>
2021-04-01 13:11:33 -0500

    Update 1539. Kth Missing Positive Number

--- facebook/leetcode/algorithms/1539.py
+++ facebook/leetcode/algorithms/1539.py
@@ -1,7 +1,9 @@
 from typing import List
 
 """1539. Kth Missing Positive Number
-Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+https://leetcode.com/problems/kth-missing-positive-number/
+
+Given an array arr of positive integers sorted in a strictly increasing order,and an integer k.
 
 Find the kth positive integer that is missing from this array.
 """
