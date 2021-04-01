4170d7432c2029a832bb611d8e1f3f7d1d50338a
alanaalfeche <alanaalfeche@gmail.com>
2021-04-01 13:25:19 -0500

    Update Problem 98: Validate Binary Search Tree

--- leetcode/blind_75/problem24.py
+++ leetcode/blind_75/problem24.py
@@ -1,24 +1,18 @@
-'''Problem 98: Validate Binary Search Tree
-
+"""Problem 98: Validate Binary Search Tree
 https://leetcode.com/problems/validate-binary-search-tree/
 
 Given a binary tree, determine if it is a valid binary search tree (BST).
 
 Assume a BST is defined as follows:
-
 The left subtree of a node contains only nodes with keys less than the node's key.
-
 The right subtree of a node contains only nodes with keys greater than the node's key.
-
 Both the left and right subtrees must also be binary search trees.
-'''
-
+"""
 
 def is_valid_bst(root):
-    '''This algorithm uses in-order traversal to get a list of all nodes in the tree. 
-
+    """This algorithm uses in-order traversal to get a list of all nodes in the tree. 
     If the list is in order, then it is a valid BST. Otherwise, return False.
-    '''
+    """
     def traverse(root, inorder):
         if not root:
             return
